59a156b8cdaa05537f82b88ba2ad3067242077d4
Henoc Díaz <henocdz@gmail.com>
2015-09-02 19:37:07 -0500

    Implementacion de ListView para listar contactos

--- address_book/settings.py
+++ address_book/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+from django.core.urlresolvers import reverse_lazy
+
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -72,6 +74,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'address_book.wsgi.application'
 
+LOGIN_URL = reverse_lazy('login_user')
 
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
